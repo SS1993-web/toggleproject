5dc01a85de9d895acfea3d7527f45e79d154a677
sneha1817 <sneha@samco.solutions>
2021-02-21 13:39:07 +0530

    first commit

---
+++ src/calculation/calculFunction.js
@@ -0,0 +1,20 @@
+
+
+var addition = function(a,b){
+    return a+b;
+}
+
+var subtraction = function(a,b){
+    return a-b;
+
+}
+
+var multiplication = function(a,b){
+     return a*b;
+
+}
+
+
+
+
+export {addition,subtraction,multiplication,};
---
+++ src/calculation/calculation.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import {addition, subtraction, multiplication,} from"./calculation.css"
+
+
+function Calculation(){
+    let [result , setResult] = useState('');
+    function  handleTextone(event) {
+        setResult(+{a:event.target.value});
+       }
+
+       function handleTexttwo(event) {
+        setResult(+{b:event.target.value});
+        }
+        
+    function buttonClickEventHandler(event){
+        setResult(event.target.value);
+        console.log(result);     
+    }
+    
+
+
+    return(
+        <div className="card">
+        
+        <h1>Calculate the Value</h1>
+       
+       <label>First value</label>
+       <input type="text" 
+       value={result.a}
+       placeholder="0"
+       onChange={handleTextone}></input><br></br>
+       <label>Second value</label>
+       <input type="text" 
+       value={result.b}
+       placeholder="0"
+       onChange={handleTexttwo}></input><br></br>
+      
+       <button onClick={buttonClickEventHandler} value={addition}>
+       Add
+       </button><br></br><br></br>
+       <button onClick={buttonClickEventHandler} value={subtraction}>
+       subtraction
+       </button><br></br><br></br>
+       <button onClick={buttonClickEventHandler} value={multiplication}>
+       multiplication
+       </button><br></br><br></br>
+       <button onClick={() => setResult(0)}>Clear Count</button>
+       <h1>Result is :{result}</h1>
+       
+        </div>
+    );
+
+}
+
+export default Calculation;
